7075c49882e3fa1b47e4b25245e03fdabb523dca
leaflu0315 <johnson7511333@gmail.com>
2018-11-30 20:31:13 +0800

    print the product via webpage

--- temp/print.php
+++ temp/print.php
@@ -6,25 +6,34 @@
     <form method = "post">
         <input type = "submit" name = "test" id = "itest">
     </form>
+
     <?php
         header("Content-Type:text/html; charset=big5");
         function myPrint($str){
             $printer = "WP-K617 Ver.3.10";  
             $ph = printer_open($printer); 
-            // $content = "�n��BIG5�s�X!!";
+            // $content = "print chinese use BIG5!";
             printer_write($ph, $str);
             printer_close($ph);
         }
 
-        if(isset($_POST)){
-            $file = "test.txt";
-            $f = fopen($file,"r");
-            $str = "";
-            while(!feof($f)){
-                $str .= fgets($f);
-            }
-            fclose($f);
-            myPrint($str);
+        if(isset($_POST['test'])){
+            // $file = "http://140.123.94.145/zhbot/myText.txt";
+            // $file2 = "http://localhost/myText.txt";
+            // $f = fopen($file,"r");
+            // iconv("UTF-8","gbk//TRANSLIT",$f);
+            // $str = "";
+            // while(!feof($f)){
+            //     $str .= fgets($f);
+            // }
+            $url='http://140.123.94.145/zhbot/myText.html';
+            $html= file_get_contents($url);
+            echo $html;
+            // fclose($f);
+            // myPrint($str);
+            myPrint($html);
+            
+            // echo $str;
         }
     ?>
     </body>
